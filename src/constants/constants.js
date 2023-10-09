31c724b7152103234aed39871b9ee1b4acad9d11
Jor <you@example.com>
2023-10-09 11:05:01 -0300

    New Changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,50 @@
-export const projects = [{
-        title: 'PHP CMS Demo',
-        description: "CMS creado en PHP y Bootstrap que permite la creación de usuarios nuevos y posts. Se aplican conocimientos como autenticación, busquedas, envio de emails.",
-        image: '/images/1.png',
-        tags: ['HTML', 'CSS', 'PHP', 'Bootstrap', 'MySQL'],
-        source: 'https://google.com',
-        visit: 'https://google.com',
-        id: 0,
-    },
-    {
-        title: 'Webpage de Restaurante',
-        description: "Webpage realizada utilizando conocimientos en HTML, CSS, Javascript y JQUERY para animaciones",
-        image: '/images/2.png',
-        tags: ['HTML', 'CSS', 'JavaScript', 'Jquery'],
-        source: 'https://google.com',
-        visit: 'https://google.com',
-        id: 1,
-    },
-    {
-        title: 'React CRUD',
-        description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-        image: '/images/3.jpg',
-        tags: ['React', 'WebRTC'],
-        source: 'https://google.com',
-        visit: 'https://google.com',
-        id: 2,
-    },
-    {
-        title: 'Java',
-        description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-        image: '/images/4.jpg',
-        tags: ['React', 'ChatEngine', 'Firebase'],
-        source: 'https://google.com',
-        visit: 'https://google.com',
-        id: 3,
-    },
+export const projects = [
+  {
+    title: "Content Managment System (CMS) Posts",
+    description:
+      "CMS creado en PHP, MySQL y Bootstrap que permite la administración y creación de usuarios nuevos, posts, comentarios y likes. Se aplican conocimientos como autenticación de usuarios, busquedas, envio de emails, likes de posts, etc.",
+    image: "/images/CMS_image.png",
+    tags: ["HTML", "CSS", "PHP", "Bootstrap", "MySQL"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Webpage de Restaurante",
+    description:
+      "Webpage realizada utilizando conocimientos en HTML, CSS, Javascript y JQUERY para animaciones",
+    image: "/images/2.png",
+    tags: ["HTML", "CSS", "JavaScript", "Jquery"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "React CRUD",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Java",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
 ];
 
 export const TimeLineData = [
-    { year: 2017, text: 'Started my journey', },
-    { year: 2018, text: 'Worked as a freelance developer', },
-    { year: 2019, text: 'Founded JavaScript Mastery', },
-    { year: 2020, text: 'Shared my projects with the world', },
-    { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
